09be52f9d4980868075fc880c62453420b5cacf5
rijeshkumar <67953156+rijeshkumar@users.noreply.github.com>
2021-01-13 13:05:05 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,15 +1,12 @@
 # Just change the info below and rename this file to config.py
 
 from pyrogram import filters
+import os
 
-API_ID = 1234567
-API_HASH = "ab1c23def45fg67890h123i45678j9kl"
-TOKEN = "1234567890:ABCdEFgHij1KlMNop_QrStuVWxyzuA-EmXI"
-SUDO_USERS = [
-    383407735,
-    951435494,
-    1392620345
-]
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+TOKEN = os.environ['TOKEN']
+SUDO_USERS = [int(admin) for admin in os.environ.get("ADMINS").split(" ")]
 LOG_GROUP = None  # Just keep it like this if you are not going to use one
 
 # No need to touch this
